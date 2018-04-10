be79bce5e568f8da2702ee5c241b86bae6d706c7
aimeelally <aimeelally@hotmail.com>
2018-04-10 16:51:51 +0700

    beta camel caser

--- src/views/camel-caser/CamelCaserController.js
+++ src/views/camel-caser/CamelCaserController.js
@@ -5,18 +5,66 @@ angular.module("camelCaser", [])
 		controller: [ '$timeout', '$scope',
 			function CamelCaserController($timeout, $scope) {
 				
+        $scope.stripWhiteSpace = true;
+        $scope.stripSpecialCharacters = false;
         $scope.inputText = '';
         $scope.outputText = '';
 
-        $scope.submit = function() {
-          getObj($scope.inputText);
+        $scope.startStrip = function() {
+          strip($scope.inputText);
         }
 
-        function getObj(str) {
-          var output = stripSpacesAndCapitalise(str);
-          $scope.outputText = stripSpecialCharacters(output);
+        function strip(str) {
+          //var output = stripSpacesAndCapitalise(str);
+          //$scope.outputText = stripSpecialCharacters(output);
 
-          return $scope.outputText;
+          // if ($scope.stripWhiteSpace) {
+          //   stripSpacesAndCapitalise(str)
+          // }
+
+          // return $scope.outputText;
+
+
+          //*********************
+
+          
+          // if nothing selected
+          if (!$scope.stripWhiteSpace && !$scope.stripSpecialCharacters) {
+            // return nothing
+            // in future return warning to select something
+            return;
+          } 
+
+          // if strip white space AND special chars
+          if ($scope.stripWhiteSpace && $scope.stripSpecialCharacters) {
+            // strip whitespace
+            // strip special chars
+            // return output
+            var output = stripSpacesAndCapitalise(str);
+            $scope.outputText = stripSpecialCharacters(output);
+            return $scope.outputText;
+          }
+
+          // if strip white spaces
+          if ($scope.stripWhiteSpace) {
+            // strip white spaces
+            // return output
+            $scope.outputText = stripSpacesAndCapitalise(str);
+            return $scope.outputText;
+          }
+            
+
+          // if strip special chars
+          if ($scope.stripSpecialCharacters) {
+            // strip special chars
+            // return output
+            $scope.outputText = stripSpecialCharacters(str);
+            return $scope.outputText;
+          } 
+
+
+            
+
         }
 
         function stripSpacesAndCapitalise(str) {
