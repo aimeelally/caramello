a2018868d34ea2326b6a3e9f450dc4e204b3615a
John Craddock <jtcraddock@gmail.com>
2018-04-19 13:54:36 +0100

    update unit tests

--- mocha/tests.js
+++ mocha/tests.js
@@ -1,42 +1,38 @@
-import CamelService from '../src/services/CamelService.js';
+import Caramel from '../src/classes/Caramel.js';
 var test = window.unitjs;
-describe('CamelService', function(){
-  it('Return an array containing a function', function(){
-    test.assert(typeof CamelService[0] === 'function');
+var caramel = new Caramel();
+describe('Caramel', function(){
+  it('Should be a function', function(){
+    test.assert(typeof Caramel === 'function');
   });
 
   it('Should strip simple html', function(){
-    var service = CamelService[0]();
     var sampleHTML = '<p>this is a paragraph</p>';
-    var result = service.stripHTML(sampleHTML);
+    var result = caramel.stripHTML(sampleHTML);
     test.assert(result === 'this is a paragraph');
   });
 
   it('Should capitalise the first letter of a string', function(){
-    var service = CamelService[0]();
     var sampleString = 'lorem ipsum something something';
-    var result = service.capitaliseFirstLetter(sampleString);
+    var result = caramel.capitaliseFirstLetter(sampleString);
     test.assert(result === 'Lorem ipsum something something');
   });
 
   it('Should convert a string to camel case', function(){
-    var service = CamelService[0]();
     var sampleString = 'lorem ipsum something something';
-    var result = service.convertToCamelCase(sampleString);
+    var result = caramel.convertToCamelCase(sampleString);
     test.assert(result === 'loremIpsumSomethingSomething');
   });
 
   it('Should strip numbers from a string', function(){
-    var service = CamelService[0]();
     var sampleString = 'lorem ips8um somet3hing somet6hing';
-    var result = service.stripNumbers(sampleString);
+    var result = caramel.stripNumbers(sampleString);
     test.assert(result === 'lorem ipsum something something');
   });
 
   it('Should strip special characters from a string', function(){
-    var service = CamelService[0]();
     var sampleString = 'lorem ips!@£$%^&*()_+-=#[]{}\'\"|<>?,./um something something';
-    var result = service.stripSpecialCharacters(sampleString);
+    var result = caramel.stripSpecialCharacters(sampleString);
     test.assert(result === 'lorem ipsum something something');
   });
   
