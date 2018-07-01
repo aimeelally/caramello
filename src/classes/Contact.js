a4061e495ed5c6fbeeda492b25fb157bc004599d
aimeelally <aimeelally@hotmail.com>
2018-07-01 20:27:24 +0700

    added functioning contact form

--- src/classes/Contact.js
+++ src/classes/Contact.js
@@ -1,46 +1,50 @@
 export default function(){
   var that =  {
-                
                 sendMessage: sendMessage
-
               };
 
 
   function sendMessage(form) {
-    var LPAWS = {};
-
-    // Initialize the Amazon Cognito credentials provider
-    AWS.config.region = 'us-east-1'; // Region
-    AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-        IdentityPoolId: 'us-east-1:54c6da73-e1de-481f-ad08-c29c7b3401b9',
-    });
-
-    AWS.config.credentials.get(function(err) {
-      if (err) {
-        console.log('1: '+err);  // an error occurred
-      }
-      else {
-        //console.log('2: '+AWS.config.credentials); // successful response
-      }
-    });
-
-    var sns = new AWS.SNS();
-    var params = {
-        Message: form, /* required */
-        Subject: 'Browser SNS publish - contact form',
-        TopicArn: 'arn:aws:sns:us-east-1:519664206156:contact-aimeelally'
-    };
-    sns.publish(params, function(err, data) {
-      if (err) {
-        console.log('3: '+err, err.stack);  // an error occurred
-      }
-      else {
-        //turn into a promise and add a success message
-        //console.log('4: '+data); // successful response
-      }     
-    });
+    return initializeAWSconfig()
+      .then(function() {
+        return sendFormToAWS(form)
+                .then( result => result )
+                .catch( error => error );
+      })
+      .catch( error => error );
   }
 
+  function initializeAWSconfig() {
+    return new Promise((resolve, reject) => {
+      var LPAWS = {};
+
+      // Initialize the Amazon Cognito credentials provider
+      AWS.config.region = 'us-east-1'; // Region
+      AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+          IdentityPoolId: 'us-east-1:54c6da73-e1de-481f-ad08-c29c7b3401b9',
+      });
+
+      AWS.config.credentials.get(function(err) {
+        return err ? reject(err) : resolve();
+      });
+    });
+  } 
+
+  function sendFormToAWS(form) {
+    return new Promise((resolve, reject) => {
+      var sns = new AWS.SNS();
+      var params =  {
+                      Message: form, /* required */
+                      Subject: 'Browser SNS publish - contact form',
+                      TopicArn: 'arn:aws:sns:us-east-1:519664206156:contact-aimeelally'
+                    };
+
+      sns.publish(params, function(err, data) {
+        return err ? reject(err) : resolve(); 
+      });
+
+    }); 
+  }
  
   return that;
 
